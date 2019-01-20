8f81ad7e7efad03c11aa25084ecbb5c867f7c3fa
MonkeyDProgramLee <44746069+MonkeyDProgramLee@users.noreply.github.com>
2019-01-20 21:36:24 +0800

    Add files via upload

---
+++ homework.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int Fibonacci(int i) {
+	//项数小于0
+	//项数小于三的情况
+	if (i < 3) {
+		return 1;
+	}
+	return Fibonacci(i - 1) + Fibonacci(i-2);
+}
+int UFib(int i) {
+	if (i < 0) {
+		printf("输入错误\n");
+	}
+	if (i < 3) {
+		return 1;
+	}
+	int now = 0;
+	int pre = 1;
+	int pre_p = 1;
+	while (i > 2)
+	{
+		now = pre + pre_p;
+		pre_p = pre;
+		pre = now;
+		  --i;
+	}
+	return now ;
+}
+int Power(int n, int k) {
+	if (n < 0) {
+		printf("错误\n");
+	}
+	if (k == 0) {
+		return 1;
+	}
+	if (k == 0 && n > 0) {
+		return 1;
+	}
+	if (n > 0 && k == 1) {
+		return n;
+	}
+	return Power(n, k - 1)*n;
+}
+int DigitSum(int number) {
+	int sum = 0;
+	if (number < 0) {
+		printf("输入有误\n");
+	}
+	int i = number / 10;
+	if (i != 0) {
+		sum=DigitSum(i)+sum;
+	}
+	//printf("%d\n", number % 10);
+	sum = sum + number % 10;
+	return sum;
+}
+int Factorial(int n) {
+	if (n == 0) {
+		printf("错误\n");
+	}
+	if (n == 1) {
+		return 1;
+	}
+	return Factorial(n - 1)*n;
+}
+int factorial(int n) {
+	int ret = 1;
+	while (n > 1) {
+		ret *= n;
+		--n;
+	}
+	return ret;
+}
+void GetBit(int i) {
+	int m;
+	m = i / 10;
+	if (m > 0) {
+		GetBit(m);
+	}
+	printf("%d\n",i%10);
+}
+int main() {
+	/*int ret2 = fibonacci(1);
+	int ret3 = fibonacci(4);
+	int ret4 = ufib(1);
+	int ret5 = ufib(4);*/
+	//int ret6 = Power(2, 0);
+	//int ret6 = factorial(3);
+	GetBit(564);
+	/*printf("%d\n", ret2);
+	printf("%d\n", ret3);
+	printf("%d\n", ret4);
+	printf("%d\n", ret5);*/
+	//printf("%d\n", ret6);
+	system("pause");
+	return 0;
+}
